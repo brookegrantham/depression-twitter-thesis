7412cda713fe59670e422e8d0e28641ceb51a84d
brookegrantham <bg17893@bristol.ac.uk>
2022-08-23 17:08:13 +0100

    test

--- testfile.sh
+++ testfile.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 #
-#
+#SBATCH --partition=test
 #SBATCH --nodes=1
 #SBATCH --ntasks-per-node=1
 #SBATCH --cpus-per-task=1
 #SBATCH --time=0:02:0
 #SBATCH --mem=100M
 
-cd /user/home/bg17893
+cd "${SLURM_SUBMIT_DIR}"
 
 python3 testbc4.py
